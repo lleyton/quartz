b1603a1954059667471181fd59912a7d7e12aa94
Adam <adam@atiktech.co>
2019-11-26 23:01:43 -0600

    :art: Added Docs for 2.0.0

--- index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  QuartzClient: require('./QuartzClient'),
-  QuartzError: require('./util/QuartzError'),
-
-  Base: require('./structures/Base'),
-  Command: require('./structures/Command'),
-  Event: require('./structures/Event'),
-  Embed: require('./structures/Embed')
-}
